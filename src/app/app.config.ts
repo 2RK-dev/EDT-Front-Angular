7accc0722a2b8d2d5ec69135cd32cb1e096377bb
rivon0507 <flavienrivon@gmail.com>
2026-02-25 16:13:56 +0300

    chore: add error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,11 +6,13 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { baseUrlInterceptor } from '@core/http/base-url-interceptor';
 import { authInterceptor } from '@core/http/auth-interceptor';
 import { refreshInterceptor } from '@core/http/refresh-interceptor';
+import { AppErrorHandler } from '@core/errors/error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor, authInterceptor, refreshInterceptor])),
-    provideRouter(routes)
+    provideRouter(routes),
+    {provide: ErrorHandler, useClass: AppErrorHandler},
   ]
 };
