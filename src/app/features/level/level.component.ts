cceba53c6fb956ecbac3f8f4e73c0282f73493b6
RyuXiu <ryanlai2486@gmail.com>
2026-02-28 13:02:14 +0300

    feat(level): implement modal for adding and editing levels with form handling

--- src/app/features/level/level.component.ts
+++ src/app/features/level/level.component.ts
@@ -5,13 +5,16 @@ import {LevelService} from '@core/domains/level/level.service';
 import {GroupService} from '@core/domains/group/group.service';
 import {LucideAngularModule, SquarePen, Trash2} from 'lucide-angular';
 import {ButtonComponent} from '@shared/components/button/button.component';
+import {Level, LevelPost} from '@core/domains/level/level.model';
+import {LevelFormComponent} from '@features/level/components/form-level/level-form.component';
+import {ModalComponent} from '@shared/components/modal/modal.component';
 
 type ViewMode = 'levels' | 'groups';
 
 @Component({
   selector: 'app-level-container',
   standalone: true,
-  imports: [CommonModule, TableComponent, LucideAngularModule, ButtonComponent],
+  imports: [CommonModule, TableComponent, LucideAngularModule, ButtonComponent, LevelFormComponent, ModalComponent],
   templateUrl: './level.component.html',
   styleUrls: ['./level.component.css']
 })
@@ -20,6 +23,9 @@ export class LevelComponent implements OnInit {
   levels$ ;
   groups$;
   isLoading$;
+
+  isModalOpen = false;
+  selectedLevel: Level | null = null;
 
   readonly SquarePen= SquarePen;
   readonly Trash2= Trash2;
@@ -52,5 +58,35 @@ export class LevelComponent implements OnInit {
 
   switchView(view: ViewMode) {
     this.currentView = view;
+  }
+
+  openAddModal() {
+    this.selectedLevel = null;
+    this.isModalOpen = true;
+  }
+
+  openEditModal(level: Level): void {
+    this.selectedLevel = level;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.selectedLevel = null;
+  }
+
+  onSaveLevel(levelData: LevelPost): void {
+    if (this.selectedLevel) {
+      this.levelService.updateLevel(this.selectedLevel.id, levelData);
+    } else {
+      this.levelService.createLevel(levelData);
+    }
+    this.closeModal();
+  }
+
+  onDeleteLevel(level: Level): void {
+    if (confirm(`Êtes-vous sûr de vouloir supprimer le niveau "${level.name}" ?`)) {
+      this.levelService.deleteLevel(level.id);
+    }
   }
 }
